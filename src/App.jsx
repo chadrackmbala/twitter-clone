fa273a48c4b35363aac306af3a5db90744245ccf
chadrackmbala <chadrackmbala20@gmail.com>
2023-12-06 12:27:40 +0000

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import LeftSidebar from "./components/left-sidebar.jsx";
+import RightSidebar from "./components/right-sidebar.jsx";
 
 import('./style/reset.css');
 import('./style/App.css');
@@ -7,8 +9,9 @@ import('./style/App.css');
 export default function App() {
   return (
     <Layout>
+      <LeftSidebar />
       <Home />
+      <RightSidebar />
     </Layout>
   );
 }
-//comment
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/header';
+import TweetEditor from '../components/tweetEditor'
+import Tweets from '../components/tweets';
 
 function Home() {
   return (
     <main className="timeline">
-      <h1>Timeline here</h1>
     </main>
   );
 }
