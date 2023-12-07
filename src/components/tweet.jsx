94b1146e1d98c9bcd960e260d0d1f3c70220c2c5
chadrackmbala <chadrackmbala20@gmail.com>
2023-12-07 04:46:47 -0800

    added components for the home section

---
+++ src/components/tweet.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+export default function Tweet(props) {
+    console.log(props.tweet.tweetGroup);
+    return (
+        <>
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <img src={props.tweet.tweetAvatar} alt="" />
+                </div>
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <div className="tweet-title">
+                            <h3>{props.tweet.tweetTitle}</h3>
+                            <img src={props.tweet.tweetGroup} alt="" />
+
+                            <p className="gray-text">{props.tweet.tweetP}</p>
+                        </div>
+                        <p className="tweet-text">
+                        {props.tweet.tweetText}
+                        </p>
+                        <img src={props.tweet.tweetImage} className="tweet-image"  />
+                    </div>
+                    {/* <TweetActions /> */}
+                    <div className="tweet-actions">
+                        <div className="tweet-action">
+                            <img src={props.tweet.tweetComment} alt="" />
+                            <p>{props.tweet.details.comments}</p>
+                        </div>
+                        <div className="tweet-action">
+                            <img src={props.tweet.tweetFlech} alt="" />
+                            <p>{props.tweet.details.fleche}</p>
+                        </div>
+                        <div className="tweet-action">
+                            <img src={props.tweet.tweetHeart} alt="" />
+                            <p>{props.tweet.details.likes}</p>
+                        </div>
+                        <div className="tweet-action">
+                            <img src={props.tweet.tweetShare} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
